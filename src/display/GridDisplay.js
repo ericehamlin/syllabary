d9157c23cbeba817434b74e8a76045322d680361
Eric Hamlin <ehamlin@grio.com>
2017-07-25 17:52:24 -0700

    fix opacity issue

--- src/display/GridDisplay.js
+++ src/display/GridDisplay.js
@@ -9,16 +9,8 @@ export default class GridDisplay {
 		this.display.setAttribute("class", "grid-display");
 
 		this.layers = [];
-		this.fadeLayers = [];
 		for (let z=1; z <= Syllabary.zDim; z++) {
-			if (z>1) {
-				let fadeLayer = document.createElement("div");
-				fadeLayer.setAttribute("class", "fade-layer");
-				fadeLayer.setAttribute("id", "fade-layer-" + (z-1));
-				fadeLayer.style.zIndex = (((Syllabary.zDim - z) * 2) + 1 );
-				this.display.appendChild(fadeLayer);
-				this.fadeLayers[z-1] = fadeLayer;
-			}
+
 			let layer = new LayerDisplay(z);
 
 			this.display.appendChild(layer.display);
@@ -35,21 +27,6 @@ export default class GridDisplay {
 	}
 
 	render() {
-		let zOpacity; // from 0.25 to 0.5
-		// TODO this might be backwards
-		if (Syllabary.grid.zPosition >= 0) {
-			zOpacity = 0.5 - (0.25 * (Syllabary.grid.zPosition % 1));
-		}
-		else {
-			// TODO negative opacity still not working
-			zOpacity = 0.25 - (0.25 * (Syllabary.grid.zPosition % 1));
-		}
-
-		for (let z in this.fadeLayers) {
-			this.fadeLayers[z].style.opacity = zOpacity;
-		}
-
-
 		for(let z in this.layers) {
 			this.layers[z].render();
 		}
--- src/display/LayerDisplay.js
+++ src/display/LayerDisplay.js
@@ -3,10 +3,21 @@
 export default class LayerDisplay {
 
 	constructor(z) {
-		this.display = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		this.display.setAttribute("class", "layer-display");
-		this.display.setAttribute("id", "layer-display-"+z);
-		this.display.setAttribute("viewBox", "-100, -100, 700, 700");
+		this.display = document.createElement("div");
+		this.display.setAttribute("class", "layer");
+
+		this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		this.svg.setAttribute("class", "layer-display");
+		this.svg.setAttribute("id", "layer-display-"+z);
+		this.svg.setAttribute("viewBox", "-100, -100, 700, 700");
+		this.display.appendChild(this.svg);
+
+		this.fadeLayer = document.createElement("div");
+		this.fadeLayer.setAttribute("class", "fade-layer");
+		this.fadeLayer.setAttribute("id", "fade-layer-" + (z-1));
+		this.fadeLayer.style.zIndex = 2;
+		this.display.appendChild(this.fadeLayer);
+
 		// if (z>1) {
 		// 	this.display.setAttribute("style", "visibility:hidden;");
 		// }
@@ -17,7 +28,7 @@ export default class LayerDisplay {
 		for (let x in Syllabary.grid.syllables) {
 			for (let y in Syllabary.grid.syllables[x]) {
 				let syllable = Syllabary.grid.syllables[x][y][this.z];
-				this.display.appendChild(syllable.glyph.glyph);
+				this.svg.appendChild(syllable.glyph.glyph);
 			}
 		}
 	}
@@ -28,13 +39,20 @@ export default class LayerDisplay {
 		this.display.style.zIndex = this.getZIndex();
 
 		// Scale layer according to depth
-		let calculated = this.getExactZPosition();
+		let exactZPosition = this.getExactZPosition();
+		let calculated = Math.pow(exactZPosition/2, 2);
 		let displacement = calculated/-2 +"%";
 		let scale = (100 + calculated) +"%";
 		this.display.style.top = displacement;
 		this.display.style.left = displacement;
 		this.display.style.width = scale;
 		this.display.style.height = scale;
+
+		console.log("EXACTZ", exactZPosition);
+
+		let visibles = 5;
+
+		this.fadeLayer.style.opacity = Math.abs(exactZPosition + 1 - Syllabary.zDim) / visibles;
 
 		// Reposition glyphs on layer according to x, y position
 		for (let x in Syllabary.grid.syllables) {
@@ -53,10 +71,10 @@ export default class LayerDisplay {
 		let zIndex;
 		let zOffset = this.getZOffset();
 		if (Syllabary.grid.zPosition >= 0) {
-			zIndex = (Syllabary.zDim - Math.floor(this.z + Syllabary.grid.zPosition) + zOffset) * 2;
+			zIndex = (Syllabary.zDim - Math.floor(this.z + Syllabary.grid.zPosition) + zOffset);
 		}
 		else {
-			zIndex = (Syllabary.zDim - Math.ceil(this.z + Syllabary.grid.zPosition) + zOffset) * 2;
+			zIndex = (Syllabary.zDim - Math.ceil(this.z + Syllabary.grid.zPosition) + zOffset);
 		}
 		return zIndex;
 	}
@@ -68,7 +86,7 @@ export default class LayerDisplay {
 	getExactZPosition() {
 		let zOffset = this.getZOffset();
 		let zPosition = Syllabary.zDim - (this.z + Syllabary.grid.zPosition) + zOffset;
-		return Math.pow(zPosition/2, 2);
+		return zPosition;
 	}
 
 	/**
