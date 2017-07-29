41e60219f4fae931c844c8a1763598b773a76237
Eric Hamlin <ehamlin@grio.com>
2017-07-28 18:33:08 -0700

    extract run method into RunController

---
+++ src/RunController.js
@@ -0,0 +1,188 @@
+'use strict';
+
+import Syllabary from "./Syllabary.js";
+
+export default class RunController {
+
+	constructor(syllabary) {
+		this.runStates = {
+			"READ" : "read", 		// audio is currently playing
+			"DRAG" : "drag",		// user is controlling using touch gesture
+			"CONTROL" : "control",	// user is controlling using provided control
+			"DRIFT" : "drift",		// user has released drag
+			"ANIMATE" : "animate"	// standard animation is advancing or drift has come to an end
+		};
+
+		this.syllabary = syllabary;
+
+		this.runState = this.runStates.ANIMATE;
+
+		this.animateInterval = 0.05;
+		this.animateDirection = {x:0, y:0, z:this.animateInterval};
+	}
+
+	run() {
+		switch(this.runState) {
+			case this.runStates.READ :
+				break;
+
+			case this.runStates.DRAG :
+				break;
+
+			case this.runStates.CONTROL :
+				break;
+
+			case this.runStates.DRIFT :
+				this.drift();
+				break;
+
+			case this.runStates.ANIMATE :
+				this.animate();
+				break;
+		}
+
+		setTimeout(() => {this.run(); }, 10);
+	}
+
+	animate() {
+		let oldXPosition = Syllabary.grid.xPosition,
+			oldYPosition = Syllabary.grid.yPosition,
+			oldZPosition = Syllabary.grid.zPosition;
+
+		Syllabary.grid.xPosition += this.animateDirection.x;
+		Syllabary.grid.yPosition += this.animateDirection.y;
+		Syllabary.grid.zPosition += this.animateDirection.z;
+		this.renderGrid();
+
+		if (
+			!this.isMovingFromSyllablePosition(oldXPosition, oldYPosition, oldZPosition) &&
+			this.animationHasCrossedSyllablePosition(oldXPosition, oldYPosition, oldZPosition)
+		) {
+
+			Syllabary.grid.xPosition = Math.round(Syllabary.grid.xPosition);
+			Syllabary.grid.yPosition = Math.round(Syllabary.grid.yPosition);
+			Syllabary.grid.zPosition = Math.round(Syllabary.grid.zPosition);
+
+			this.renderGrid();
+
+			console.info("ending animate");
+			this.runState = this.runStates.READ;
+			this.read();
+		}
+	}
+
+	isMovingFromSyllablePosition(xPosition, yPosition, zPosition) {
+		return (
+			Math.floor(xPosition) == xPosition &&
+			Math.floor(yPosition) == yPosition &&
+			Math.floor(zPosition) == zPosition
+		);
+	}
+
+	animationHasCrossedSyllablePosition(oldXPosition, oldYPosition, oldZPosition) {
+		return (
+			Math.floor(oldXPosition) != Math.floor(Syllabary.grid.xPosition) ||
+			Math.floor(oldYPosition) != Math.floor(Syllabary.grid.yPosition) ||
+			Math.floor(oldZPosition) != Math.floor(Syllabary.grid.zPosition)
+		);
+	}
+
+	/**
+	 * TODO combine drift and animate so that drift goes to a glyph
+	 */
+	drift() {
+		let drag = 0.95;
+
+		Syllabary.grid.xPosition += this.animateDirection.x;
+		Syllabary.grid.yPosition += this.animateDirection.y;
+		Syllabary.grid.zPosition += this.animateDirection.z;
+		this.renderGrid();
+
+
+		this.animateDirection.x = this.animateDirection.x * drag;
+		this.animateDirection.y = this.animateDirection.y * drag;
+		this.animateDirection.z = this.animateDirection.z * drag;
+
+		if (this.getAnimateVelocity() < 0.01) {
+			console.info("ending drift");
+			this.runState = this.runStates.ANIMATE;
+		}
+	}
+
+	getAnimateVelocity() {
+		let xSqr = Math.pow(this.animateDirection.x, 2);
+		let ySqr = Math.pow(this.animateDirection.y, 2);
+		let zSqr = Math.pow(this.animateDirection.z, 2);
+		return Math.sqrt(xSqr + ySqr + zSqr);
+	}
+
+	read() {
+		// get current syllable
+		let x = this.getCurrentLocation(Syllabary.grid.xPosition, Syllabary.xDim);
+		let y = this.getCurrentLocation(Syllabary.grid.yPosition, Syllabary.yDim);
+		let z = this.getCurrentLocation(Syllabary.grid.zPosition, Syllabary.zDim);
+
+		console.info("READING %s-%s-%s", x, y, z);
+		let syllable = Syllabary.grid.syllables[x][y][z];
+		let promise = syllable.play();
+		promise.then(() => {
+			this.setNewAnimateDirection();
+		this.runState = this.runStates.ANIMATE;
+	});
+
+	}
+
+	/**
+	 * randomly assign new direction
+	 * TODO make sure we're not going backward
+	 */
+	setNewAnimateDirection() {
+		let direction = Math.floor(Math.random() * 6); // this calculation is incorrect. it will not be evenly distributed
+		let xInterval = 0,
+			yInterval = 0,
+			zInterval = 0;
+		switch(direction) {
+			case 0:
+				xInterval = this.animateInterval;
+				break;
+			case 1:
+				xInterval = -this.animateInterval;
+				break;
+			case 2:
+				yInterval = this.animateInterval;
+				break;
+			case 3:
+				yInterval = -this.animateInterval;
+				break;
+			case 4:
+				zInterval = this.animateInterval;
+				break;
+			case 5:
+			default:
+				zInterval = -this.animateInterval;
+				break;
+		}
+		this.animateDirection = {x:xInterval, y:yInterval, z:zInterval};
+	}
+
+	renderGrid() {
+		this.syllabary.syllabaryDisplay.render();
+	}
+
+	/**
+	 * TODO not a good name
+	 *
+	 * @param position
+	 * @param dim
+	 * @returns {number}
+	 */
+	getCurrentLocation(position, dim) {
+		if (position >= 0) {
+			return position - (Math.floor(position/dim) * dim) + 1;
+		}
+		else {
+			return position - (Math.ceil(position/dim) * dim) + 1;
+		}
+
+	}
+}
--- src/Syllabary.js
+++ src/Syllabary.js
@@ -4,6 +4,7 @@ import Grid from './Grid.js';
 import GlyphLoader from './GlyphLoader.js';
 import LoadingDisplay from './display/LoadingDisplay.js';
 import SyllabaryDisplay from './display/SyllabaryDisplay.js';
+import RunController from './RunController.js';
 import * as Hammer from "hammerjs";
 
 export default class Syllabary {
@@ -29,16 +30,6 @@ export default class Syllabary {
 
 		/** this probably shouldn't be a class variable, but it needs to be globally available */
 		Syllabary.grid = new Grid(xPosition, yPosition, zPosition);
-
-		Syllabary.runStates = {
-			"READ" : "read", 		// audio is currently playing
-			"DRAG" : "drag",		// user is controlling using touch gesture
-			"CONTROL" : "control",	// user is controlling using provided control
-			"DRIFT" : "drift",		// user has released drag
-			"ANIMATE" : "animate"	// standard animation is advancing or drift has come to an end
-		};
-
-		Syllabary.animateInterval = 0.05;
 
 		this.initialize();
 	}
@@ -70,10 +61,6 @@ export default class Syllabary {
 				that.syllabaryDisplay.render();
 				that.syllabaryDisplay.add();
 
-				that.runState = Syllabary.runStates.ANIMATE;
-
-				that.animateDirection = {x:0, y:0, z:Syllabary.animateInterval};
-
 				let el = document.getElementById(Syllabary.containerId);
 				let hammer = new window.Hammer(el);
 				hammer.get('pinch').set({ enable: true });
@@ -100,167 +87,11 @@ export default class Syllabary {
 	 * TODO move all this stuff to a controller
 	 */
 	run() {
-
-		switch(this.runState) {
-			case Syllabary.runStates.READ :
-				break;
-
-			case Syllabary.runStates.DRAG :
-				break;
-
-			case Syllabary.runStates.CONTROL :
-				break;
-
-			case Syllabary.runStates.DRIFT :
-				this.drift();
-				break;
-
-			case Syllabary.runStates.ANIMATE :
-				this.animate();
-				break;
-		}
+		let controller = new RunController(this);
+		controller.run();
 
 		// continue regularly until some unforseen event takes place, in which case,
 		// this.complete()
-		setTimeout(() => {this.run(); }, 10);
-
-	}
-
-	animate() {
-		let oldXPosition = Syllabary.grid.xPosition,
-			oldYPosition = Syllabary.grid.yPosition,
-			oldZPosition = Syllabary.grid.zPosition;
-
-		Syllabary.grid.xPosition += this.animateDirection.x;
-		Syllabary.grid.yPosition += this.animateDirection.y;
-		Syllabary.grid.zPosition += this.animateDirection.z;
-		this.syllabaryDisplay.render();
-
-		if (
-			!this.isMovingFromSyllablePosition(oldXPosition, oldYPosition, oldZPosition) &&
-			this.animationHasCrossedSyllablePosition(oldXPosition, oldYPosition, oldZPosition)
-			) {
-
-			Syllabary.grid.xPosition = Math.round(Syllabary.grid.xPosition);
-			Syllabary.grid.yPosition = Math.round(Syllabary.grid.yPosition);
-			Syllabary.grid.zPosition = Math.round(Syllabary.grid.zPosition);
-
-			this.syllabaryDisplay.render();
-
-			console.info("ending animate");
-			this.runState = Syllabary.runStates.READ;
-			this.read();
-		}
-	}
-
-	isMovingFromSyllablePosition(xPosition, yPosition, zPosition) {
-		return (
-			Math.floor(xPosition) == xPosition &&
-			Math.floor(yPosition) == yPosition &&
-			Math.floor(zPosition) == zPosition
-		);
-	}
-
-	animationHasCrossedSyllablePosition(oldXPosition, oldYPosition, oldZPosition) {
-		return (
-			Math.floor(oldXPosition) != Math.floor(Syllabary.grid.xPosition) ||
-			Math.floor(oldYPosition) != Math.floor(Syllabary.grid.yPosition) ||
-			Math.floor(oldZPosition) != Math.floor(Syllabary.grid.zPosition)
-		);
-	}
-
-	/**
-	 * TODO combine drift and animate so that drift goes to a glyph
-	 */
-	drift() {
-		let drag = 0.95;
-
-		Syllabary.grid.xPosition += this.animateDirection.x;
-		Syllabary.grid.yPosition += this.animateDirection.y;
-		Syllabary.grid.zPosition += this.animateDirection.z;
-		this.syllabaryDisplay.render();
-
-
-		this.animateDirection.x = this.animateDirection.x * drag;
-		this.animateDirection.y = this.animateDirection.y * drag;
-		this.animateDirection.z = this.animateDirection.z * drag;
-
-		if (this.getAnimateVelocity() < 0.01) {
-			console.info("ending drift");
-			this.runState = Syllabary.runStates.ANIMATE;
-		}
-	}
-
-	getAnimateVelocity() {
-		let xSqr = Math.pow(this.animateDirection.x, 2);
-		let ySqr = Math.pow(this.animateDirection.y, 2);
-		let zSqr = Math.pow(this.animateDirection.z, 2);
-		return Math.sqrt(xSqr + ySqr + zSqr);
-	}
-
-	read() {
-		// get current syllable
-		let x = this.getCurrentLocation(Syllabary.grid.xPosition, Syllabary.xDim);
-		let y = this.getCurrentLocation(Syllabary.grid.yPosition, Syllabary.yDim);
-		let z = this.getCurrentLocation(Syllabary.grid.zPosition, Syllabary.zDim);
-
-		console.info("READING %s-%s-%s", x, y, z);
-		let syllable = Syllabary.grid.syllables[x][y][z];
-		let promise = syllable.play();
-		promise.then(() => {
-			this.setNewAnimateDirection();
-			this.runState = Syllabary.runStates.ANIMATE;
-		});
-
-	}
-
-	/**
-	 * randomly assign new direction
-	 * TODO make sure we're not going backward
-	 */
-	setNewAnimateDirection() {
-		let direction = Math.floor(Math.random() * 6); // this calculation is incorrect. it will not be evenly distributed
-		let xInterval = 0,
-			yInterval = 0,
-			zInterval = 0;
-		switch(direction) {
-			case 0:
-				xInterval = Syllabary.animateInterval;
-				break;
-			case 1:
-				xInterval = -Syllabary.animateInterval;
-				break;
-			case 2:
-				yInterval = Syllabary.animateInterval;
-				break;
-			case 3:
-				yInterval = -Syllabary.animateInterval;
-				break;
-			case 4:
-				zInterval = Syllabary.animateInterval;
-				break;
-			case 5:
-			default:
-				zInterval = -Syllabary.animateInterval;
-				break;
-		}
-		this.animateDirection = {x:xInterval, y:yInterval, z:zInterval};
-	}
-
-	/**
-	 * TODO not a good name
-	 *
-	 * @param position
-	 * @param dim
-	 * @returns {number}
-	 */
-	getCurrentLocation(position, dim) {
-		if (position >= 0) {
-			return position - (Math.floor(position/dim) * dim) + 1;
-		}
-		else {
-			return position - (Math.ceil(position/dim) * dim) + 1;
-		}
 
 	}
 
