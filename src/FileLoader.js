344f1499ff0d494546191d2f8bc9ff343404448f
Eric Hamlin <ehamlin@grio.com>
2021-09-25 18:38:11 -0700

    comment

--- src/FileLoader.js
+++ src/FileLoader.js
@@ -3,11 +3,10 @@
 export default class FileLoader {
 	static load(filename) {
 		return new Promise(function(resolve, reject) {
-
-			let xhttp = new XMLHttpRequest();
+			const xhttp = new XMLHttpRequest();
 			xhttp.overrideMimeType('text/xml; charset=CP1252'); // Windows Latin-1 Charset
 			xhttp.onreadystatechange = function () {
-				if (this.readyState == 4) {
+				if (this.readyState == 4) { // 4 = DONE
 					if (this.status == 200) {
 						resolve(this.response);
 					}
