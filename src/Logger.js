c73d98c5b18f8e8e3c0161371b891a128d6ca88d
Eric Hamlin <ehamlin@grio.com>
2021-10-16 14:58:23 -0700

    complete logger

--- src/Logger.js
+++ src/Logger.js
@@ -3,24 +3,34 @@
 import Config from './Config.js';
 
 export const LogLevels = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    debug: 3
+  error: 0,
+  warn: 1,
+  info: 2,
+  debug: 3
 };
 
 const Logger = {
-    debug: (...args) => {
-        if (Config.logLevel === LogLevels.debug) {
-            console.debug(...args)
-        }
-    },
-
-    info: (...args) => {
-        if (Config.logLevel >= LogLevels.info) {
-
-        }
+  debug: (...args) => {
+    if (Config.logLevel === LogLevels.debug) {
+      console.debug(...args)
     }
+  },
+
+  info: (...args) => {
+    if (Config.logLevel >= LogLevels.info) {
+      console.info(...args)
+    }
+  },
+
+  warn: (...args) => {
+    if (Config.logLevel >= LogLevels.warn) {
+      console.warn(...args);
+    }
+
+  },
+  error: (...args) => {
+    console.error(...args);
+  },
 };
 
 export default Logger;
